fdd1a952ba42227d8c699f4d5f464b773221ff4a
Dominik Russenberger <dominik.russenberger@students.fhnw.ch>
2026-02-03 22:37:59 +0100

    same name, different namespace

--- doit.sh
+++ doit.sh
@@ -11,6 +11,10 @@ setfattr -n "security.nsecurity" -v bar 3
 setfattr -n "trusted.ntrusted" -v bar 3
 setfattr -n "system.nsystem" -v bar 3
 
+touch 4
+setfattr -n "gnu.foo" -v gnu 4
+setfattr -n "user.foo" -v user 4
+
 echo "---- attr 1:"
 attr -l 1
 attr -g "foo" 1
@@ -19,18 +23,27 @@ attr -l 2
 attr -g "foo" 2
 attr -g "baz" 2
 echo "---- attr 3:"
-attr -l 2
 attr -l 3
+echo "---- attr 4:"
+attr -l 4
+attr -g "foo" 4
 
 echo "---- getfattr 1:"
 getfattr -d 1
 echo "---- getfattr 1 all:"
 getfattr -m - -d 1
+
 echo "---- getfattr 2:"
 getfattr -d 2
 echo "---- getfattr 2 all:"
 getfattr -m - -d 2
+
 echo "---- getfattr 3:"
 getfattr -d 3
 echo "---- getfattr 3 all:"
 getfattr -m - -d 3
+
+echo "---- getfattr 4:"
+getfattr -d 4
+echo "---- getfattr 4 all:"
+getfattr -m - -d 4
